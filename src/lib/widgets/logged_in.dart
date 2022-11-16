25474cbae7de97c1a1ee3e658a58785580420e68
SethCohen <sethcohen.dev@gmail.com>
2022-11-16 17:00:00 -0500

    feat: added fully functional login/logout system using Google authentication and Firebase
    closes #3

--- src/lib/widgets/logged_in.dart
+++ src/lib/widgets/logged_in.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/google_provider.dart';
 
 class LoggedIn extends StatefulWidget {
   const LoggedIn({Key? key}) : super(key: key);
@@ -42,8 +45,12 @@ class _LoggedInState extends State<LoggedIn> {
             style: TextButton.styleFrom(
               foregroundColor: Theme.of(context).colorScheme.onPrimary,
             ),
-            onPressed: () {},
-            child: const Text('Profile'),
+            onPressed: () {
+              final provider =
+              Provider.of<GoogleSignInProvider>(context, listen: false);
+              provider.googleLogout();
+            },
+            child: const Text('Sign Out'),
           ),
         ],
       ),
