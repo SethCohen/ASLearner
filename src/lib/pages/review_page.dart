20f5725797ee418d29c78c4d0db094ed34f1b445
Seth Cohen <sethcohen.dev@gmail.com>
2023-02-22 15:19:30 -0500

    style: changed review button width

--- src/lib/pages/review_page.dart
+++ src/lib/pages/review_page.dart
@@ -15,65 +15,71 @@ class _ReviewPageState extends State<ReviewPage> {
   @override
   Widget build(BuildContext context) {
     // TODO get all cards from user's in progress collection and allow the user to review over them either by lesson or by all cards
-    return StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection("lessons").snapshots(),
-        builder: (context, snapshot1) {
-          if (snapshot1.hasData) {
-            return StreamBuilder<QuerySnapshot>(
-              stream: FirebaseFirestore.instance
-                  .collection("users")
-                  .doc(user.uid)
-                  .collection("progress")
-                  .snapshots(),
-              builder: (context, snapshot2) {
-                if (snapshot2.hasData) {
-                  final lessons = snapshot1.data!.docs;
-                  final progress = snapshot2.data!.docs;
+    return Center(
+      child: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance.collection("lessons").snapshots(),
+          builder: (context, snapshot1) {
+            if (snapshot1.hasData) {
+              return StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance
+                    .collection("users")
+                    .doc(user.uid)
+                    .collection("progress")
+                    .snapshots(),
+                builder: (context, snapshot2) {
+                  if (snapshot2.hasData) {
+                    final lessons = snapshot1.data!.docs;
+                    final progress = snapshot2.data!.docs;
 
-                  return ListView.builder(
-                    itemCount: lessons.length,
-                    itemBuilder: (context, index) {
-                      final lesson = lessons[index];
-                      final lessonId = lesson.id;
-                      final lessonTitle = _convertIdToTitle(lessonId);
+                    return SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.33,
+                      child: ListView.builder(
+                        itemCount: lessons.length,
+                        itemBuilder: (context, index) {
+                          final lesson = lessons[index];
+                          final lessonId = lesson.id;
+                          final lessonTitle = _convertIdToTitle(lessonId);
 
-                      if (!progress.any((element) => element.id == lessonId)) {
-                        FirebaseFirestore.instance
-                            .collection("users")
-                            .doc(user.uid)
-                            .collection("progress")
-                            .doc(lessonId)
-                            .set({"complete": false});
-                      }
+                          if (!progress
+                              .any((element) => element.id == lessonId)) {
+                            FirebaseFirestore.instance
+                                .collection("users")
+                                .doc(user.uid)
+                                .collection("progress")
+                                .doc(lessonId)
+                                .set({"complete": false});
+                          }
 
-                      return Card(
-                        child: ListTile(
-                            title: Text(lessonTitle),
-                            trailing: const Icon(Icons.check),
-                            onTap: () => Navigator.pushNamed(
-                                  context,
-                                  '/lesson',
-                                  arguments: {
-                                    'lesson': lesson,
-                                    'isReview': true,
-                                  },
-                                )),
-                      );
-                    },
-                  );
-                } else if (snapshot2.hasError) {
-                  return const Center(child: Text('Something went wrong!'));
-                } else {
-                  return const Center(child: CircularProgressIndicator());
-                }
-              },
-            );
-          } else if (snapshot1.hasError) {
-            return const Center(child: Text('Something went wrong!'));
-          } else {
-            return const Center(child: CircularProgressIndicator());
-          }
-        });
+                          return Card(
+                            child: ListTile(
+                                title: Text(lessonTitle),
+                                trailing: const Icon(Icons.check),
+                                onTap: () => Navigator.pushNamed(
+                                      context,
+                                      '/lesson',
+                                      arguments: {
+                                        'lesson': lesson,
+                                        'isReview': true,
+                                      },
+                                    )),
+                          );
+                        },
+                      ),
+                    );
+                  } else if (snapshot2.hasError) {
+                    return const Text('Something went wrong!');
+                  } else {
+                    return const CircularProgressIndicator();
+                  }
+                },
+              );
+            } else if (snapshot1.hasError) {
+              return const Text('Something went wrong!');
+            } else {
+              return const CircularProgressIndicator();
+            }
+          }),
+    );
   }
 
   String _convertIdToTitle(String input) {
